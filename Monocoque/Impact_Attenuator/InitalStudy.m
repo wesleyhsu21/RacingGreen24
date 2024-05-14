8134ef66e90a2f4d7ac6d0ef1e9ab7782ca95722
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-05-14 13:03:15 +0100

    Initial values

--- Monocoque/Impact_Attenuator/InitalStudy.m
+++ Monocoque/Impact_Attenuator/InitalStudy.m
@@ -1,3 +1,13 @@
 clear
 clc
 
+%% Honeycomb
+
+% Aluminium Properties
+sigma_Y_solid_Al5052 = 193e6;       % Yield Strength in Pa
+sigma_UTS_solid_Al5052 = 228e6;     % Ultimiate Tensilte Strength in Pa
+E_solid_Al5052 = 70.3e9;            % Young's Modulus in Pa
+G_solid_Al5052 = 25.9e6;            % Shear Modulus in Pa
+ShearStrength_solid_Al5052 = 138e6; % Shear Stress in Pa
+rho_solid_Al5052 = 2680;            % Density in kg/m^3
+nu_solid_Al5052 = 0.33;             % Poisson Ratio
