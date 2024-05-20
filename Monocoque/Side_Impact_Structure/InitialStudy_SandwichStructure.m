1585701288376df653b8cbf3f359a510f2cb0229
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-05-20 11:16:53 +0100

    Assuming specially orthotropic facesheet

--- Monocoque/Side_Impact_Structure/InitialStudy_SandwichStructure.m
+++ Monocoque/Side_Impact_Structure/InitialStudy_SandwichStructure.m
@@ -19,59 +19,41 @@ clc
 layup_iteration_0_s = [0 0 0 -45 45 0 0 0 90 0 0 0 -45 45];
 [A_0, B_0, D_0, ABD_0, Q_0, thickness_0] = ABD(layup_iteration_0_s);
 
-z_max_0 = thickness_0/2;% Max z-coordinate value
+L = 1000e-3;% Length of the structure in m
+b = 240e-3;% Depth of the beam, i.e. about the axis of bending in m
 
-%% 3-point bending
+%% Orthoropic approximation
 F = 1000;% Force in N
-% Force loading matrix
-N = [0;
-    F;
-    0];
-M_max = F * Length / 2;% Magnitude of Max moment in Nm
 
-% Assuming specially orthotrpic, B = 0
-
-% Moment/unit length vector
-M = [M_max / Width;
-    0;
-    0];
-
-% Curvature vector
-K = D_0 \ M;
-
-%% Applying ABD
-epsilon0_0 = A_0 \ N;
-
-% Assuming worst case strain
-epsilon_0 = epsilon0_0 + z_max_0 * K;
-
-sigma_0 = N / thickness_0;
-
-E_f = abs(sigma_0 ./ epsilon_0);% Modulus of the facesheet in Pa
-
-
+E_f_x = (1 - A_0(1,2)^2 / (A_0(2,2) * A_0(1,1))) * A_0(1,1) / b;
+E_f_y = (1 - A_0(1,2)^2 / (A_0(2,2) * A_0(1,1))) * A_0(2,2) / b;
 %% Dimensions and properties
-E_c = ;% Modulus of the foam core in Pa
-b = ;% Depth of the beam, i.e. about the axis of bending in m
-c = ;% Thickness of the foam core
+E_c = psiToPa(22e3);% Modulus of the foam core in Pa
+c = 30e-3;% Thickness of the foam core
 d = c + thickness_0;
-G_c = ;% Shear modulus of the foam core in Pa
-L = ;% Length of the structure in m
-sigma_critical_fc = ;% Compressive composite facesheet failure stress in Pa
-tau_critical_glue = ;% Glue delamination stress
-tau_critical_core = ;% Critical core shear stress
-sigma_critical_core = tau_critical_core;% Critical stress same due to isotropic
+G_c = psiToPa(10e3);% Shear modulus of the foam core in Pa
+sigma_critical_fc = 1.780e9;% Compressive composite facesheet failure stress in Pa
+tau_critical_glue = 60e6;% Glue delamination stress
+tau_critical_core = psiToPa(95);% Critical core shear stress
+sigma_critical_core = psiToPa(115);% Critical stress same due to isotropic
+rho_fc = 1570;
+rho_c = 28.8;
+volume_fc = thickness_0 * b * L;% Volume in m^3
+mass_fc = volume_fc * rho_fc;% Mass in kg
+volume_c = c * b * L;% Volume in m^3
+mass_c = volume_c * rho_c;% Mass in kg
+mass_total = mass_fc + mass_c
 
 %% Dimensionless quantities
 tbar = thickness_0 / c;
 cbar = c / L;
 sigmabar = sigma_critical_fc / sigma_critical_core;
 taubar = tau_critical_core / sigma_critical_fc;
-Ebar = E_f / simga_critical_fc;
+Ebar = E_f_x / sigma_critical_fc;
 Fbar = F / (b * L * sigma_critical_fc);
 
 %% Equivalent bending stiffness
-EI_sw = (E_f * b * t * d^2 / 2) + (E_f * b * t^3 / 6) + (E_c * b * c^3 / 12);
+EI_sw = (E_f_x * b * thickness_0 * d^2 / 2) + (E_f_x * b * thickness_0^3 / 6) + (E_c * b * c^3 / 12);
 
 %% Equivalent shear stiffness
 AG_sw = b * c * G_c;
@@ -80,7 +62,7 @@ AG_sw = b * c * G_c;
 delta_3point = (F * L^3) / (48 * EI_sw) + (F * L) / (4 * AG_sw);% Approximate deflection in m
 
 %% Facesheet failure
-F_critical_facesheet = (4 * d * b * t * sigma_critical_fc) / L;% Critial load in 3-point bending causing facesheet failure
+F_critical_facesheet = (4 * d * b * thickness_0 * sigma_critical_fc) / L;% Critial load in 3-point bending causing facesheet failure
 if F_critical_facesheet < F
     disp("Sandwich structure fails in facesheet failure")
 end
@@ -97,4 +79,4 @@ if tau_critical_core < tau_I
 end
 
 %% Indentation failure
-F_critical_indentation = b * t * ((pi^2 * d * E_f * sigma_critical_fc ^ 2) / (3 * L)) ^ (2/3);
+F_critical_indentation = b * thickness_0 * ((pi^2 * d * E_f_x * sigma_critical_fc ^ 2) / (3 * L)) ^ (2/3);
