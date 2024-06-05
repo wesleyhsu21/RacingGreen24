93f6cd38b4453626c40a50f04be37ce997f1feca
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-06-05 11:54:42 +0100

    f

---
+++ Monocoque/Side_Impact_Structure/FDPlotter.m
@@ -0,0 +1,27 @@
+clear
+clc
+close all
+
+% Force-Displacement Plotter
+matName = "ThreePointBendFEA.mat";
+figName = "TestFig.png";
+yLab = 'Displacement / mm';
+xLab = 'Force / N'
+
+load(matName)
+x = ThreePointBend.TestData(:,1)
+y = ThreePointBend.TestData(:,2)
+
+
+XYPlot = figure;
+plot(x,y)
+hold on
+xlabel(xLab)
+ylabel(yLab)
+hold off
+betterPlot(XYPlot)
+legend("off")
+saveas(XYPlot, figName)
+
+%% Area under
+area = trapz(x,y) * 1e-3
