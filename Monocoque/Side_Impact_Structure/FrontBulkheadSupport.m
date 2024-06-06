1bdc8cf30c8cad88cd57a3412855393813044ad0
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-06-06 17:56:22 +0100

    g

--- Monocoque/Side_Impact_Structure/FrontBulkheadSupport.m
+++ Monocoque/Side_Impact_Structure/FrontBulkheadSupport.m
@@ -17,12 +17,12 @@ close all
 % through the other facesheet
 
 %% Facesheet layup
-layup_iteration_0_s = [90 0 -45 45 0 -45 45 0 0 90 -45 45 0 0];
+layup_iteration_0_s = [0 0 90 0 0 -45 45 0 -45 +45 0 0 90 -45 45 90 0 0];
 [A_0, B_0, D_0, ABD_0, Q_0, thickness_0] = ABD(layup_iteration_0_s);
 
 L = 780e-3;% Length of the structure in m
 b = 300e-3;% Depth of the beam, i.e. about the axis of bending in m
-c = 50e-3;% Thickness of the foam core
+c = 35e-3;% Thickness of the foam core
 
 %% Dimensions and properties
 E_c = psiToPa(65e3);% Modulus of the foam core in Pa
@@ -204,3 +204,6 @@ area_test = area_test * 2
 area_covered = L * b;
 mass_per_m2 = mass_total / area_covered
 cost_per_m2 = cost / area_covered
+
+area_bulkheadsupport = 1.4;
+cost_bulkheadsupport = area_bulkheadsupport * cost_per_m2
